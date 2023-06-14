99b75469ef698a57d3271012208b134421d673d4
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-13 23:04:12 -0600

    validation email

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,6 +1,10 @@
 function handler(req, res) {
     if(req.method === 'POST') {
         const userEmail = req.body.email;
+
+        if(!userEmail || !userEmail.includes('@')) {
+            
+        }
     }
 }
 
