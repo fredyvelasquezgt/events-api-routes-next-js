98ad09b321fbcc1443ddcacd3f45f53a153dfd76
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-13 23:30:45 -0600

    finished fetch

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -17,7 +17,8 @@ function NewsletterRegistration() {
       headers: {
         'Content-Type': 'application/json'
       }
-    });
+    }).then(response => response.json())
+    .then(data => console.log(data));
   }
 
   return (
